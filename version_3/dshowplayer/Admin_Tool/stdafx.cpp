c75142e044d9d87e2187587dcede94eabebc141c
U-NCR-DSK-W10-01\ncr <ncr@automation-intelligence.com>
2019-01-10 18:07:15 +0000

    Version 1

--- version_3/dshowplayer/Admin_Tool/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Admin_Tool.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
--- version_6/web/welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('lock.php');
-?>
-<body>
-<br><a href="login.php">Login</a>  |  <a href="logout.php">Logout</a> | <a href="upload.php">Upload Video</a>  | <a href="videos.php">Videos</a></br>
-<br></br>
-<h1>Welcome <?php echo $login_session; ?></h1>
-</body>
