8da8d8fec085bc2067c9b33cfabab57e5b5c6241
U-NCR-DSK-W10-01\ncr <ncr@automation-intelligence.com>
2019-01-10 18:16:54 +0000

    cleaned up for version 7

--- version_6/web/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_SERVER', 'mysql.qtester.orinsw.com');
-define('DB_USERNAME', 'ncr_test');
-define('DB_PASSWORD', 'test1234');
-define('DB_DATABASE', 'qtester_app');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
